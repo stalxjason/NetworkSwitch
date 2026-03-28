f63c6c108646e18d73a346399aa4610e263c3f65
qvqiiiii <16664397+stalxjason@users.noreply.github.com>
2026-03-28 09:31:51 +0800

    Fix: use cmd phone set-allowed-network-types-for-users

--- app/src/main/java/com/example/networkswitch/NetworkMode.kt
+++ app/src/main/java/com/example/networkswitch/NetworkMode.kt
@@ -2,10 +2,14 @@ package com.example.networkswitch
 
 /**
  * 网络模式枚举
+ *
+ * @param label 显示名称
+ * @param telephonyType settings put global preferred_network_mode 的值
+ * @param allowedTypesMask cmd phone set-allowed-network-types-for-users 的位掩码
  */
-enum class NetworkMode(val label: String, val telephonyType: Int) {
-    LTE("4G", 21),                    // NETWORK_MODE_LTE_ONLY
-    NR_5G("5G", 33);                  // NETWORK_MODE_NR_LTE_GSM_WCDMA
+enum class NetworkMode(val label: String, val telephonyType: Int, val allowedTypesMask: String) {
+    LTE("4G", 21, "01000001000000000000"),
+    NR_5G("5G", 33, "11000001000000000000");
 
     companion object {
         fun fromTelephonyType(type: Int): NetworkMode? =
